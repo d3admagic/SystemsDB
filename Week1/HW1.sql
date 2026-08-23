22493d3185971c76c9fdb7adb94d2d3e6fe070f3
Zeke26-afk <Elee3@csi.edu>
2026-08-23 12:34:05 -0600

    Homework

---
+++ Week1/HW1.sql
@@ -0,0 +1,17 @@
+Select current_timestamp;
+Select current_database();
+Select version();
+Select 'Hello SQL!'
+
+-- A database is created by using "Create Databass ___;"
+
+-- A table is a group of data like employees in a work place, it's created by using"create table()"
+-- EX: Creat Table Employees (
+--			id bigserial,
+--			first_name(),
+--			last_name(),
+--			Employee_id
+--		);
+
+-- Semicolons wrap up the code, it let the quary tool know that the code is finished.
+-- Bigserial and varchar

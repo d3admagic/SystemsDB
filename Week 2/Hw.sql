a871e5a950fca0362422f05c422ce04603a42aea
d3admagic <ezjalee42@gmail.com>
2026-09-21 11:30:37 -0600

    week 6 lab

---
+++ Week 2/Hw.sql
@@ -0,0 +1,30 @@
+-- step 1
+CREATE DATABASE PracticeDB;
+
+-- step 2
+
+CREATE TABLE Video_Games (
+	table_id BIGSERIAL PRIMARY KEY,
+	Title VARCHAR(50),
+	Genres VARCHAR(50),
+	Playtime INTEGER,
+	Release_date DATE
+);
+
+-- step 3 & 4
+
+INSERT INTO Video_Games (Title, Genres, Playtime, Release_date)
+	VALUES ('Cyberpunk 2077', 'ARPG', '480','2020-12-10' ),
+	('Fable', 'ARPG', '500', '2008-10-21' ),
+	('Viewtiful joe', 'Beat-em up', '100', '2003-06-24')
+
+	--> step 5 
+	('Overwatch' 'shooter' '2016-24-05');
+--ERROR:  syntax error at or near "("
+--LINE 7:  ('Overwatch' 'shooter' '2016-24-05');
+
+
+
+
+-- One new concept that I learned about this week was about DISTINCT, distinct takes out values that repeat
+-- I think WHERE is going to be the hardest thing to wrap my head around. I'm not sure I 100% understand it all the way
---
+++ Week 6/lab 6.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE products_100(id_Index integer PRIMARY KEY, product_name VARCHAR(100), Description VARCHAR(100),
+-- Brand Varchar(100), category text, price numeric(10,2), Currency text, Stock integer, EAN VARCHAR(100), Color text,
+-- size VARCHAR(100), Availability text, Intermal_ID integer);
+
+-- SELECT price, Stock, price*Stock AS inventory_value
+-- FROM products_100
+
+-- SELECT AVG(price) AS AVG_price
+-- FROM products_100
+
+-- SELECT MIN(price), MAX(price)
+-- FROM products_100
+
+-- SELECT COUNT(*)
+-- From products_100
